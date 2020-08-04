bffb04f9344a98a8d5d6c87d12200ac5e02b64ae
GonzalezExequiel <gonzalez-exequiel@hotmail.com>
2020-08-04 00:54:49 -0300

    PexelBanner modified ir order to be scalable for different screen sizes

--- lib/widgets/pexel_banner.dart
+++ lib/widgets/pexel_banner.dart
@@ -6,14 +6,13 @@ class PexelBanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 24,
       color: Colors.white,
       child: Center(
         child: ClipRRect(
           child: BackdropFilter(
             filter: ImageFilter.blur(sigmaX: 100, sigmaY: 100),
             child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 1),
               child: TextUrl(
                 url: 'https://www.pexels.com',
                 text: "Photos provided by Pexels",
