e722b3938a4a48dc920f9c0fa6146e2e8dd1b7b5
GonzalezExequiel <gonzalez-exequiel@hotmail.com>
2020-08-06 00:34:05 -0300

    app signed

--- lib/ads.dart
+++ lib/ads.dart
@@ -2,14 +2,14 @@ class Ads {
   static String appId = 'ca-app-pub-8405433680296878/9623035199';
 
   //real banner id
-  //  static String bannerId = 'ca-app-pub-8405433680296878/9623035199';
+  static String bannerId = 'ca-app-pub-8405433680296878/9623035199';
 
 //test banner id
-  static String bannerId = 'ca-app-pub-3940256099942544/6300978111';
+//  static String bannerId = 'ca-app-pub-3940256099942544/6300978111';
 
   //real rewarded id
-//static String rewardId = 'ca-app-pub-8405433680296878/7921288324';
+  static String rewardId = 'ca-app-pub-8405433680296878/7921288324';
 
 //test rewarded id
-  static String rewardId = 'ca-app-pub-3940256099942544/5224354917';
+//  static String rewardId = 'ca-app-pub-3940256099942544/5224354917';
 }
